d46715c4e62c9a71a0522384747205d79485394f
kpopova7 <kr.popova518@gmail.com>
2023-01-15 02:07:14 +0200

    -list now has working add and remove option!

--- src/routes/aList.js
+++ src/routes/aList.js
@@ -2,70 +2,88 @@ import React, { useState } from 'react';
 
 import './aList.scss';
 
-// This holds a list of some fiction people
-// Some  have the same name but different age and id
-const USERS = [
-  { name: 'Andy'},
-  { name: 'Bob'},
-  { name: 'Tom Hulk'},
-  { name: 'Audra'},
-  { name: 'Anna'},
-  { name: 'Tom'},
-  { name: 'Tom Riddle'},
-  { name: 'Bolo'},
-];
+const USERS = [];
 
 function AList() {
-  // the value of the search field 
-  const [name, setName] = useState('');
+    var timestamp = (new Date()).getTime();
 
-  // the search result
-  const [foundUsers, setFoundUsers] = useState(USERS);
+    // the value of the search field 
+    const [name, setName] = useState('');
 
-  const filter = (e) => {
-    const keyword = e.target.value;
+    // all users
+    const [allUsers, setAllUsers] = useState(USERS);
 
-    if (keyword !== '') {
-      const results = USERS.filter((user) => {
-        return user.name.toLowerCase().startsWith(keyword.toLowerCase());
-        // Use the toLowerCase() method to make it case-insensitive
-      });
-      setFoundUsers(results);
-    } else {
-      setFoundUsers(USERS);
-      // If the text field is empty, show all users
+    // the search result
+    const [foundUsers, setFoundUsers] = useState(USERS);
+
+    const filter = (e) => {
+        const keyword = e.target.value;
+
+        if (keyword !== '') {
+            const results = allUsers.filter((user) => {
+            return user.name.toLowerCase().startsWith(keyword.toLowerCase());
+            });
+            setFoundUsers(results);
+        } else {
+            setFoundUsers(allUsers);
+        }
+
+        setName(keyword);
+    };
+
+    const deleteById = id => {
+        const newUsers = allUsers.filter((person) => person.id !== id);
+        setAllUsers(newUsers);
+        setFoundUsers(newUsers);
     }
-
-    setName(keyword);
-
-  };
-
-  return (
-    <div className="container">
-      <input
-        value={name}
-        onChange={filter}
-        className="input"
-        placeholder="Filter"
-      />
-
-
-      <div className="user-list">
-        {foundUsers && foundUsers.length > 0 ? (
-          foundUsers.map((user) => (
-            <li key={user.id} className="user">
-              <span className="user-name">{user.name}</span>
-            </li>
-          ))
-        ) : (
-          <h1>No results found!</h1>
-        )}
-      </div>
-    </div>
-  );
-
-
-  
+    return (
+        <div className="container">
+            <input
+                value={name}
+                onChange={filter}
+                className="input"
+                placeholder="Filter"
+            />
+            <div className="user-list">
+                {foundUsers && foundUsers.length > 0 ? (
+                    foundUsers.map((user) => (
+                    <li key={user.id} className="user">
+                        <span className="user-name">{user.name} and {user.id}</span>
+                        <button onClick={() => deleteById(user.id) }>X</button>
+                    </li>
+                    ))
+                ) : (
+                    <div>
+                        {allUsers.length == 0 ? (
+                            <div>
+                                <div>emptyy</div>
+                                <button onClick={() => {
+                                setName('');
+                                allUsers.push({
+                                  name: name,
+                                  id: timestamp
+                                });
+                                setFoundUsers(allUsers);
+                                }}>Add</button>
+                            </div>
+                        ) : (
+                            <div>
+                                <h1>No results found!</h1>
+                                <button onClick={() => {
+                                setName('');
+                                allUsers.push({
+                                  name: name,
+                                  id: timestamp
+                                });
+                                setFoundUsers(allUsers);
+                                }}>Add</button>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default AList;
